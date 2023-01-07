ad7d51e37c1a060b5f3d78e414e33de9d06ec6f2
Thomascogez <thomas.cogez@gmail.com>
2023-01-08 00:16:51 +0100

    chore(test): improve waitForMaildev

--- __tests__/helpers/waitForMaildev.ts
+++ __tests__/helpers/waitForMaildev.ts
@@ -2,8 +2,7 @@ import supertest from "supertest";
 import { MAILDEV_API_ENDPOINT } from "../constants/mailDev";
 
 export const waitForMaildev = async (retries = 10): Promise<void> => {
-    const response = await supertest(MAILDEV_API_ENDPOINT)
-        .get("/email");
+    const response = await supertest(MAILDEV_API_ENDPOINT).get("/healthz");
 
     if (response.statusCode === 200) return;
 

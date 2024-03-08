11b00bd47419f5c97eb73c4eccf68cf515e134b9
Adri Van Houdt <adri.van.houdt@gmail.com>
2024-03-08 17:57:04 +0100

    feat: escape html by default

--- __tests__/unit/buildMjmlTemplate.spec.ts
+++ __tests__/unit/buildMjmlTemplate.spec.ts
@@ -2,58 +2,102 @@ import { join } from "path";
 import { buildMjmlTemplate } from "../../src";
 
 describe("buildMjmlTemplate", () => {
-
-    it("should fail if template folder does not exist", async () => {
-        await expect(buildMjmlTemplate({
-            templateFolder: join(__dirname, "folderThatDoesNotExist"),
-        }, { templateName: "templateThatDoesNotExist" })).rejects.toThrow();
-    });
-
-    it("should fail if template does not exist", async () => {
-        await expect(buildMjmlTemplate({
-            templateFolder: join(__dirname, "../resources"),
-        }, { templateName: "templateThatDoesNotExist" })).rejects.toThrow();
-    });
-
-    it("should fail if mjml template is invalid", async () => {
-        await expect(buildMjmlTemplate({
-            templateFolder: join(__dirname, "../resources"),
-        }, { templateName: "test-invalid" })).rejects.toThrow();
-    });
-
-    it("should compile a template without template data", async () => {
-        const mailHtmlContent = await buildMjmlTemplate({
-            templateFolder: join(__dirname, "../resources"),
-        }, { templateName: "test" });
-
-        expect(mailHtmlContent).toBeDefined();
-    });
-
-    it("should compile a template with template data and html content should be replaced", async () => {
-        const mailHtmlContent = await buildMjmlTemplate({
-            templateFolder: join(__dirname, "../resources"),
+  it("should fail if template folder does not exist", async () => {
+    await expect(
+      buildMjmlTemplate(
+        {
+          templateFolder: join(__dirname, "folderThatDoesNotExist"),
         },
-            {
-                templateName: "test-mustache",
-                templateData: {
-                    testKey: "testKey",
-                    testKeyNested: {
-                        nestedKey: "nestedKey"
-                    }
-                }
-            });
+        { templateName: "templateThatDoesNotExist" }
+      )
+    ).rejects.toThrow();
+  });
 
-        expect(mailHtmlContent).toBeDefined();
-        expect(mailHtmlContent).toContain("testKey");
-        expect(mailHtmlContent).toContain("nestedKey");
-    });
+  it("should fail if template does not exist", async () => {
+    await expect(
+      buildMjmlTemplate(
+        {
+          templateFolder: join(__dirname, "../resources"),
+        },
+        { templateName: "templateThatDoesNotExist" }
+      )
+    ).rejects.toThrow();
+  });
 
-    it("should compile a template using mjml-include tag", async () => {
-        const mailHtmlContent = await buildMjmlTemplate({
-            templateFolder: join(__dirname, "../resources"),
-        }, {templateName: "test-include/test-include"});
+  it("should fail if mjml template is invalid", async () => {
+    await expect(
+      buildMjmlTemplate(
+        {
+          templateFolder: join(__dirname, "../resources"),
+        },
+        { templateName: "test-invalid" }
+      )
+    ).rejects.toThrow();
+  });
 
-        expect(mailHtmlContent).toBeDefined();
-        expect(mailHtmlContent).toContain("This is a header");
-    });
+  it("should compile a template without template data", async () => {
+    const mailHtmlContent = await buildMjmlTemplate(
+      {
+        templateFolder: join(__dirname, "../resources"),
+      },
+      { templateName: "test" }
+    );
+
+    expect(mailHtmlContent).toBeDefined();
+  });
+
+  it("should compile a template with template data and html content should be replaced", async () => {
+    const mailHtmlContent = await buildMjmlTemplate(
+      {
+        templateFolder: join(__dirname, "../resources"),
+      },
+      {
+        templateName: "test-mustache",
+        templateData: {
+          testKey: "testKey",
+          testKeyNested: {
+            nestedKey: "nestedKey",
+          },
+        },
+      }
+    );
+
+    expect(mailHtmlContent).toBeDefined();
+    expect(mailHtmlContent).toContain("testKey");
+    expect(mailHtmlContent).toContain("nestedKey");
+  });
+
+  it("should compile a template with html content and escape that by default", async () => {
+    const mailHtmlContent = await buildMjmlTemplate(
+      {
+        templateFolder: join(__dirname, "../resources"),
+      },
+      {
+        templateName: "test-mustache",
+        templateData: {
+          testKey: "<h1>testKey</h1>",
+          testKeyNested: {
+            nestedKey: "nestedKey",
+          },
+        },
+      }
+    );
+
+    expect(mailHtmlContent).toBeDefined();
+    console.log(mailHtmlContent);
+    expect(mailHtmlContent).toContain("&lt;h1&gt;testKey&lt;&#x2F;h1&gt;");
+    expect(mailHtmlContent).toContain("nestedKey");
+  });
+
+  it("should compile a template using mjml-include tag", async () => {
+    const mailHtmlContent = await buildMjmlTemplate(
+      {
+        templateFolder: join(__dirname, "../resources"),
+      },
+      { templateName: "test-include/test-include" }
+    );
+
+    expect(mailHtmlContent).toBeDefined();
+    expect(mailHtmlContent).toContain("This is a header");
+  });
 });
